f6ecfd2d5ef8f63a0452d46714f542be5ea401ba
Youjin Chung <krista2811@kaist.ac.kr>
2021-02-22 12:30:30 +0900

    [#2] Added Header Icons, need to link - links

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,19 @@
 import React from "react"
-import {makeStyles} from "@material-ui/core"
+import { Icon, makeStyles } from "@material-ui/core"
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import HomeIcon from '@material-ui/icons/'
+import HomeIcon from '@material-ui/icons/Home'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import CodeIcon from '@material-ui/icons/Code'
+import PersonIcon from '@material-ui/icons/Person'
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks'
 
 const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
   appBar: {
 
   },
@@ -16,7 +23,24 @@ export default function Header() {
   return (
     <AppBar position={"static"}>
       <Toolbar>
-
+        <Typography variant={"h5"} className={classes.title}>
+          Genne
+        </Typography>
+        <IconButton color={"inherit"}>
+          <HomeIcon />
+        </IconButton>
+        <IconButton color={"inherit"}>
+          <GitHubIcon />
+        </IconButton>
+        <IconButton color={"inherit"}>
+          <CodeIcon />
+        </IconButton>
+        <IconButton color={"inherit"}>
+          <PersonIcon />
+        </IconButton>
+        <IconButton color={"inherit"}>
+          <LibraryBooksIcon />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
